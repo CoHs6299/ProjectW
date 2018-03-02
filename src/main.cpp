83bd4a3ed30bc4b70dc6a35523fb67d5987a12b9
Tommy Wong <cohs6299@hotmail.com>
2018-03-02 15:30:22 +0800

    Add some test code to verify that ESP8266 works correctly.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
-
+  pinMode(6, HIGH);
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  for (size_t i = 0; i < 5; i++) {
+    pinMode(6, LOW);
+    delay(6);
+  }
 }
